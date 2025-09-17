da953fb04d8de0d7324a85b9beb92fc2be5185f8
José Manuel Mclanghlin Matienzo <mclanghlin@gmail.com>
2025-09-17 11:41:11 -0300

    Add subnet and gateway outputs to VPC module
    Added outputs for public_subnet_ids, private_subnet_ids, and internet_gateway_id in both the VPC module and the development environment. Also included descriptions for all outputs in the VPC module for better clarity.

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,15 +1,23 @@
+# ID de la VPC creada
 output "vpc_id" {
-  value = aws_vpc.this.id
+  description = "ID de la VPC"
+  value       = aws_vpc.this.id
 }
 
+# IDs de las subnets públicas
 output "public_subnet_ids" {
-  value = [for subnet in aws_subnet.public : subnet.id]
+  description = "IDs de las subnets públicas"
+  value       = [for subnet in aws_subnet.public : subnet.id]
 }
 
+# IDs de las subnets privadas
 output "private_subnet_ids" {
-  value = [for subnet in aws_subnet.private : subnet.id]
+  description = "IDs de las subnets privadas"
+  value       = [for subnet in aws_subnet.private : subnet.id]
 }
 
+# ID del Internet Gateway
 output "internet_gateway_id" {
-  value = aws_internet_gateway.this.id
+  description = "ID del Internet Gateway"
+  value       = aws_internet_gateway.this.id
 }
