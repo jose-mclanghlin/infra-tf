20b8a4dcd1c87312fc6358b624efcd92f643e71d
José Manuel Mclanghlin Matienzo <mclanghlin@gmail.com>
2025-10-28 17:10:31 -0300

    Remove EC2 module and related configuration
    Deleted the EC2 module from the codebase, including its Terraform files and all related variable and configuration references in the development environment. This simplifies the infrastructure by removing EC2 provisioning and associated outputs, variables, and user data.

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -9,33 +9,10 @@ module "vpc" {
 
 module "ec2" {
   source = "../../modules/ec2"
-  
-  instance_name               = var.ec2_instance_name
-  instance_type              = var.ec2_instance_type
-  key_name                   = var.ec2_key_name
-  subnet_id                  = module.vpc.public_subnets[0]  # Using first public subnet
-  associate_public_ip_address = var.ec2_associate_public_ip
-  volume_size                = var.ec2_volume_size
-  environment                = "development"
-  
-  tags = {
-    Project     = "plub"
-    Environment = "development"
-    Terraform   = "true"
-  }
-  
-  # Basic user data script to install updates
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-    echo "<h1>Hello from Development EC2!</h1>" > /var/www/html/index.html
-    EOF
+   # ...specific variables of the ec2 module
 }
 
 module "rds" {
   source = "../../modules/rds"
-  # ...specific variables of the RDS module
+ 
 }
--- environments/development/variables.tf
+++ environments/development/variables.tf
@@ -37,34 +37,3 @@ variable "azs" {
     error_message = "Debes especificar al menos una availability zone."
   }
 }
-
-# EC2 Variables
-variable "ec2_instance_name" {
-  description = "Name for the EC2 instance"
-  type        = string
-  default     = "plub-use1-dev-ec2"
-}
-
-variable "ec2_instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t3.micro"
-}
-
-variable "ec2_key_name" {
-  description = "Key pair name for EC2 instance"
-  type        = string
-  default     = ""
-}
-
-variable "ec2_volume_size" {
-  description = "Size of EBS volume in GB"
-  type        = number
-  default     = 20
-}
-
-variable "ec2_associate_public_ip" {
-  description = "Whether to associate a public IP address"
-  type        = bool
-  default     = true
-}
