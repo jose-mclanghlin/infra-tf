0925fd7fd667199c6754da25ccca201909cd9e55
José Manuel Mc Langhlin Matienzo <mclanghlin@gmail.com>
2025-10-29 20:44:09 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@
 // See more: https://docs.aws.amazon.com/prescriptive-guidance/latest/terraform-aws-provider-best-practices/structure.html#repo-structure
 // See more: https://medium.com/@bouachirhamza/structuring-terraform-projects-like-a-pro-modules-workspaces-best-practices-92c3f47df02b
 terraform {
-  required_version = "~> 1.4.0"
+  required_version = ">= 1.4.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
