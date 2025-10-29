6032fd572a5f1d3fbbfa571e5f47d1d1e32ea715
José Manuel Mc Langhlin Matienzo <mclanghlin@gmail.com>
2025-10-29 11:06:17 -0300

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,4 @@
+// See more: https://medium.com/@sanoj.sudo/how-to-create-aws-vpc-788dc3c4193b
 resource "aws_vpc" "this" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
@@ -16,27 +17,28 @@ resource "aws_internet_gateway" "this" {
 }
 
 resource "aws_subnet" "public" {
-  count                   = length(var.public_subnets_cidr)
+  for_each                = { for idx, cidr in var.public_subnets_cidr : idx => cidr }
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = var.public_subnets_cidr[count.index]
-  availability_zone       = var.azs[count.index]
+  cidr_block              = each.value
+  availability_zone       = var.azs[each.key]
   map_public_ip_on_launch = true
   tags = {
-    Name = "${var.name}-public-${count.index + 1}"
+    Name = "${var.name}-public-${each.key + 1}"
   }
 }
 
 resource "aws_subnet" "private" {
-  count             = length(var.private_subnets_cidr) 
+  for_each          = { for idx, cidr in var.private_subnets_cidr : idx => cidr }
   vpc_id            = aws_vpc.this.id              
-  cidr_block        = var.private_subnets_cidr[count.index] 
-  availability_zone = var.azs[count.index]
+  cidr_block        = each.value
+  availability_zone = var.azs[each.key]
   map_public_ip_on_launch = false 
   tags = {
-    Name = "${var.name}-private-${count.index + 1}"
+    Name = "${var.name}-private-${each.key + 1}"
   }
 }
 
+# This route table directs internet-bound traffic from public subnets to the internet gateway
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
   tags = {
@@ -44,15 +46,17 @@ resource "aws_route_table" "public" {
   }
 }
 
+# This route allows outbound internet access from public subnets
 resource "aws_route" "public_internet_access" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0" # All IPv4 traffic
   gateway_id             = aws_internet_gateway.this.id
 }
 
+# Associates the public subnets with the public route table
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
   route_table_id = aws_route_table.public.id
 }
 
@@ -64,7 +68,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "this" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
+  subnet_id     = values(aws_subnet.public)[0].id
   tags = {
     Name = "${var.name}-nat-gw"
   }
@@ -85,7 +89,7 @@ resource "aws_route" "private_internet_access" {
 }
 
 resource "aws_route_table_association" "private" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
+  for_each       = aws_subnet.private
+  subnet_id      = each.value.id
   route_table_id = aws_route_table.private.id
 }
