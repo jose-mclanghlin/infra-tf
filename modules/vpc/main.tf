7ce00970f281001bae436ede4980674dd2189be6
José Manuel Mc Langhlin Matienzo <mclanghlin@gmail.com>
2025-10-29 10:32:07 -0300

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "public" {
   count                   = length(var.public_subnets_cidr)
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnets_cidr[count.index]
-  availability_zone       = element(var.azs, count.index)
+  availability_zone       = var.azs[count.index]
   map_public_ip_on_launch = true
   tags = {
     Name = "${var.name}-public-${count.index + 1}"
@@ -29,7 +29,7 @@ resource "aws_subnet" "private" {
   count             = length(var.private_subnets_cidr) 
   vpc_id            = aws_vpc.this.id              
   cidr_block        = var.private_subnets_cidr[count.index] 
-  availability_zone = element(var.azs, count.index) 
+  availability_zone = var.azs[count.index]
   map_public_ip_on_launch = false 
   tags = {
     Name = "${var.name}-private-${count.index + 1}"
