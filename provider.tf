4bd7649f6951ff58f2c18442627f317b5b054dca
José Manuel Mc Langhlin Matienzo <mclanghlin@gmail.com>
2025-10-29 17:03:01 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,6 @@
 // See more: https://medium.com/devsecops-community/terraform-project-structure-a-step-by-step-guide-for-scalable-infrastructure-1e51e8029849
+// See more: https://docs.aws.amazon.com/prescriptive-guidance/latest/terraform-aws-provider-best-practices/structure.html#repo-structure
+// See more: https://medium.com/@bouachirhamza/structuring-terraform-projects-like-a-pro-modules-workspaces-best-practices-92c3f47df02b
 terraform {
   required_version = ">= 1.4.0"
   required_providers {
