bc2089f7f965dd3fdfd969db3025b9fbe2d40c56
José Manuel Mc Langhlin Matienzo <mclanghlin@gmail.com>
2025-10-30 10:47:56 -0300

    Refactor DynamoDB resource and add outputs
    Renamed the DynamoDB table resource from 'terraform_locks' to 'this' for consistency. Added outputs for the table name and ARN in outputs.tf to facilitate referencing these values in other modules.

--- global/dynamo-db/main.tf
+++ global/dynamo-db/main.tf
@@ -1,13 +1,12 @@
-resource "aws_dynamodb_table" "terraform_locks" {
+resource "aws_dynamodb_table" "this" {
   name         = var.dynamodb_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
-
   attribute {
     name = "LockID"
     type = "S"
   }
-
+  
   tags = {
     Name        = var.dynamodb_table_name
     Environment = "global"
