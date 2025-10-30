cdbfb46615b4b6b3b4d99935d2c50174a53d89e9
José Manuel Mc Langhlin Matienzo <mclanghlin@gmail.com>
2025-10-30 10:45:04 -0300

    Rename dynamodb module and update references
    Renamed the 'dynamodb' module directory to 'dynamo-db' for consistency. Updated references in global/main.tf and removed unused outputs related to DynamoDB from the S3 module.

--- global/main.tf
+++ global/main.tf
@@ -1,6 +1,4 @@
 module "s3_backend" {
   source              = "./s3"
   bucket_name         = "plub-use1-terraform-state"
-  dynamodb_table_name = "plub-use1-terraform-lock"
-  environment         = "global"
 }
