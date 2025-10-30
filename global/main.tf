702fc590898b3e0f3a258354141ca26336f80c66
José Manuel Mc Langhlin Matienzo <mclanghlin@gmail.com>
2025-10-30 10:50:12 -0300

    Update main.tf

--- global/main.tf
+++ global/main.tf
@@ -2,3 +2,9 @@ module "s3_backend" {
   source              = "./s3"
   bucket_name         = "plub-use1-terraform-state"
 }
+
+module "dynamodb_table" {
+  source              = "./dynamodb"
+  environment         = "dev"
+  dynamodb_table_name = "plub-use1-terraform-locks"
+}
