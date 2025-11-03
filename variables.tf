616361d954a794453d7039a5044c56fae7fc4359
José Manuel Mc Langhlin Matienzo <mclanghlin@gmail.com>
2025-11-03 14:18:33 -0300

    Remove Terraform provider and variables, update Terragrunt config
    Deleted provider.tf and variables.tf, removing direct Terraform AWS provider and region variable configuration. Added documentation links to terragrunt.hcl, likely shifting configuration management to Terragrunt.

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region to use. Allowed values: us-east-1 (N. Virginia), us-west-1 (N. California), us-west-2 (Oregon)."
-  type        = string
-  default     = "us-east-1"
-  validation {
-    condition     = contains(["us-east-1", "us-west-1", "us-west-2"], var.aws_region)
-    error_message = "Region must be us-east-1 (N. Virginia), us-west-1 (N. California), or us-west-2 (Oregon)."
-  }
-}
