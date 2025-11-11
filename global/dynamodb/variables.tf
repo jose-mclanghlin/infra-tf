a2268d0dd7c5c9af1b057fdfb24b8f3223a5912f
José Manuel Mclanghlin Matienzo <mclanghlin@gmail.com>
2025-11-11 10:55:44 -0300

    Add Terraform modules for S3 backend and DynamoDB lock
    Introduces Terraform modules to provision an S3 bucket for remote state storage and a DynamoDB table for state locking. Includes resource definitions, variables, and outputs for both modules, and a main module to instantiate them with specific names.

---
+++ global/dynamodb/variables.tf
@@ -0,0 +1,4 @@
+variable "dynamodb_table_name" {
+  description = "Nombre de la tabla DynamoDB para locks de Terraform"
+  type        = string
+}
---
+++ global/s3/variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+  description = "Nombre del bucket S3 para almacenar los estados de Terraform"
+  type        = string
+}
