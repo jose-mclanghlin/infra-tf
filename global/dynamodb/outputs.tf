2ad9377019a2623bcd466628525daaca6e7dffb1
José Manuel Mclanghlin Matienzo <mclanghlin@gmail.com>
2025-11-11 15:08:12 -0300

    Remove global Terraform backend modules
    Deleted the global S3 and DynamoDB Terraform modules and their associated variables and outputs. This removes the infrastructure for Terraform state storage and locking from the repository.

--- global/dynamodb/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "table_name" {
-  description = "Nombre de la tabla DynamoDB creada"
-  value       = aws_dynamodb_table.this.name
-}
-
-output "table_arn" {
-  description = "ARN de la tabla DynamoDB"
-  value       = aws_dynamodb_table.this.arn
-}
